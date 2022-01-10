a217695b008ec30270d7c8478ad6c857c578c031
dengfa <dengfa@bytedance.com>
2022-01-10 18:54:30 +0800

    周日期数据

--- qmui/src/main/java/com/qmuiteam/qmui/calendar/WeekView.kt
+++ qmui/src/main/java/com/qmuiteam/qmui/calendar/WeekView.kt
@@ -4,29 +4,69 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
+import android.view.ViewGroup
 import com.qmuiteam.qmui.R
 
 /**
  * Created by dengfa on 2022/1/10
  * calendar week view
  */
-class WeekView(context: Context, attrs: AttributeSet, defStyle: Int) : View(context, attrs, defStyle) {
+class WeekView : View {
+    private val DEFAULT_SIZE: Int = 200
     private val ROWS_CNT = 24
     private var paint = Paint(Paint.ANTI_ALIAS_FLAG)
     var daysCount = 7
-
-    constructor(context: Context, attrs: AttributeSet) : this(context, attrs, 0)
+    var rawHeight = 180
 
     init {
         paint.color = context.resources.getColor(R.color.qmui_config_color_gray_1)
     }
 
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
+
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+
+        val widthSpecMode = MeasureSpec.getMode(widthMeasureSpec)
+        val widthSize = MeasureSpec.getSize(widthMeasureSpec);
+
+        val heightSpecMode = MeasureSpec.getMode(heightMeasureSpec)
+        val heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        Log.d("vincent", "widthSpecMode $widthSpecMode heightSpecMode $heightSpecMode")
+
+        // widthSpecMode 1073741824 heightSpecMode 0 （UNSPECIFIED）
+        // 嵌套在ScrollerView里面时，WrapContent对应于UNSPECIFIED，所有应该通过layoutParams判断
+        /*if (widthSpecMode == MeasureSpec.AT_MOST && heightSpecMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(DEFAULT_SIZE, rawHeight * ROWS_CNT)
+        } else if (widthSpecMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(DEFAULT_SIZE, heightSize)
+        } else if (heightSpecMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(widthSize, rawHeight * ROWS_CNT)
+        }*/
+
+
+        // 当布局参数设置为wrap_content时，设置默认值
+        if (layoutParams.width == ViewGroup.LayoutParams.WRAP_CONTENT && layoutParams.height == ViewGroup.LayoutParams.WRAP_CONTENT) {
+            setMeasuredDimension(DEFAULT_SIZE, rawHeight * ROWS_CNT);
+        } else if (layoutParams.width == ViewGroup.LayoutParams.WRAP_CONTENT) {
+            setMeasuredDimension(DEFAULT_SIZE, heightSize);
+        } else if (layoutParams.height == ViewGroup.LayoutParams.WRAP_CONTENT) {
+            setMeasuredDimension(widthSize, rawHeight * ROWS_CNT);
+        }
+    }
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        val rowHeight = 85
         for (i in 0 until ROWS_CNT) {
-            val y = rowHeight * i.toFloat()
+            val y = rawHeight * i.toFloat()
             canvas.drawLine(0f, y, width.toFloat(), y, paint)
         }
 
--- qmuidemo/src/main/java/com/qmuiteam/qmuidemo/fragment/components/calendar/WeekViewFragment.kt
+++ qmuidemo/src/main/java/com/qmuiteam/qmuidemo/fragment/components/calendar/WeekViewFragment.kt
@@ -15,19 +15,25 @@
  */
 package com.qmuiteam.qmuidemo.fragment.components.calendar
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import butterknife.BindView
 import butterknife.ButterKnife
-import com.qmuiteam.qmui.behavior.VpBottomSheetBehavior
-import com.qmuiteam.qmui.kotlin.dip
 import com.qmuiteam.qmui.kotlin.onClick
 import com.qmuiteam.qmui.widget.QMUITopBarLayout
 import com.qmuiteam.qmuidemo.R
 import com.qmuiteam.qmuidemo.base.BaseFragment
+import com.qmuiteam.qmuidemo.fragment.components.calendar.WeekHeaderAdapter.ViewHolder
 import com.qmuiteam.qmuidemo.lib.annotation.Widget
 import com.qmuiteam.qmuidemo.manager.QDDataManager
 import com.qmuiteam.qmuidemo.model.QDItemDescription
+import java.util.Calendar
+import java.util.Date
 
 @Widget(name = "CalendarWeekView", iconRes = R.mipmap.icon_grid_status_bar_helper)
 class WeekViewFragment : BaseFragment() {
@@ -35,18 +41,84 @@ class WeekViewFragment : BaseFragment() {
     @BindView(R.id.topbar)
     internal lateinit var mTopBar: QMUITopBarLayout
 
+    @BindView(R.id.rcWeekHeader)
+    internal lateinit var rcWeekHeader: RecyclerView
+
+
     private lateinit var mQDItemDescription: QDItemDescription
 
+
     override fun onCreateView(): View {
-        val view = LayoutInflater.from(activity).inflate(R.layout.fragment_bottom_sheet, null)
+        val view = LayoutInflater.from(activity).inflate(R.layout.fragment_calendar_week_view, null)
         ButterKnife.bind(this, view)
         mQDItemDescription = QDDataManager.getInstance().getDescription(this.javaClass)
         initTopBar()
+        initCalendar()
         return view
     }
 
     private fun initTopBar() {
         mTopBar.addLeftBackImageButton().onClick { popBackStack() }
         mTopBar.setTitle(mQDItemDescription.name)
+    }
+
+    private fun initCalendar() {
+        val days = arrayListOf<Calendar>()
+        val firstOfWeek = getFirstOfWeek(Date())
+        days.add(firstOfWeek)
+        Log.d("vincent", "getFirstOfWeek $firstOfWeek")
+        for (index in 1..6) {
+            val calendar = Calendar.getInstance()
+            calendar.time = firstOfWeek.time
+            calendar.add(Calendar.DAY_OF_MONTH, index)
+            days.add(calendar)
+        }
+
+        rcWeekHeader.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        val weekHeaderAdapter = WeekHeaderAdapter()
+        rcWeekHeader.adapter = weekHeaderAdapter
+        weekHeaderAdapter.setData(days)
+    }
+
+
+    private fun getFirstOfWeek(date: Date): Calendar {
+        val calendar: Calendar = Calendar.getInstance()
+        calendar.time = date
+        calendar.set(Calendar.DAY_OF_WEEK, 1)
+        return calendar
+    }
+}
+
+class WeekHeaderAdapter : RecyclerView.Adapter<ViewHolder>() {
+    private val days = arrayListOf<Calendar>()
+    val weekStr = arrayListOf("日", "一", "二", "三", "四", "五", "六")
+
+    fun setData(data: List<Calendar>) {
+        days.clear()
+        days.addAll(data)
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvWeek: TextView = itemView.findViewById(R.id.tvWeek)
+        val tvDate: TextView = itemView.findViewById(R.id.tvDate)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemWidth = parent.width / 7
+        Log.d("vincent", "itemWidth $itemWidth")
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_calendar_week_header, parent, false)
+        itemView.layoutParams.width = itemWidth
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val calendar = days[position]
+        holder.tvWeek.text = weekStr[(calendar.get(Calendar.DAY_OF_WEEK) - 1) % weekStr.size]
+        holder.tvDate.text = "${calendar.get(Calendar.DAY_OF_MONTH)}"
+    }
+
+    override fun getItemCount(): Int {
+        return days.size
     }
 }
